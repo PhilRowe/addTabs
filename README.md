22c8530ce61610b74e4ae36dbb7fd9982ef49f3d
Philip Rowe <philiprowe@hotmail.co.uk>
2014-08-30 22:02:18 +0100

    Initial commit of addTab plugin files

--- README.md
+++ README.md
@@ -1,2 +1,159 @@
-addTabs
-=======
+# addTabs()
+
+jQuery addtabs() plugin. Easily add tabs to your webpage. This simple plugin will work straight out of the box for most use cases. However if you need more power there are plenty of events and callbacks that you can hook into.
+
+* <a href="http://ionden.com/a/plugins/ion.tabs/en.html">View Demo Page</a>
+* <a href="http://ionden.com/a/plugins/ion.tabs/ion.sound-1.0.2.zip">Download addTabs now</a>
+
+## Description
+* Add many groups of tabs to a single page and initialise with one line of JavaScript
+* Configuration options for launch and reinit
+* Access the internal methods for full control
+* Cross-browser support: Google Chrome, Mozilla Firefox, Opera, Safari, IE(8.0+)
+* Simple skin by default, Customise to make it you own!
+
+## Dependencies
+* Developed using <a href="http://jquery.com/" target="_blank">jQuery 1.11.1</a>
+* Works with <a href="http://zeptojs.com/">Zepto!</a>
+
+## Usage
+
+### Installation
+
+1. Add both jQuery (if not already present) and the addTabs plugin to the head of your document
+```html
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="jquery.collapse.js"></script>
+```
+
+2. **Optional** add the addTabs CSS file (You could develop your own skin from scratch or add ours to your main style sheet and then modify it)
+```html
+<link rel="stylesheet" type="text/css" href="addTabs.css">
+```
+
+3. Add the tabs markup to your document e.g.
+```html
+<div class="tab-container addTabs">
+    <div class="tabs">
+        <a class="tab-link" data-related-tab="#tab-one">Tab One</a>
+        <a class="tab-link" data-related-tab="#tab-two">Tab Two</a>
+        <a class="tab-link" data-related-tab="#tab-three">Tab Three</a>
+    </div>
+    <div class="tab-holder">
+        <div id="tab-one" class="tab-content">
+            Tab 1 content
+        </div>
+        <div id="tab-two" class="tab-content">
+            Tab 2 Content
+        </div>
+        <div id="tab-three" class="tab-content">
+            Tab 3 Content
+        </div>
+    </div>
+</div>
+```
+
+4. Launch the plugin
+```html
+<script type="text/javascript">
+    $(document).ready(function(){
+    	$('.addTabs').addTabs();
+    });
+</script>
+```
+
+### Custom Markup and Data attributes
+You may wish to use slightly different markup and classes this can be achieved by informing addTabs of your changes either through the settings (see below) or just through the html:
+
+```html
+<div class="tab-container addTabs" data-links="ul >li" data-content=".tab-holder >div">
+    <ul>
+        <li data-related-tab="#tab-one">Tab One</li>
+        <li data-related-tab="#tab-two">Tab Two</li>
+        <li data-related-tab="#tab-three">Tab Three</li>
+    </ul>
+    <div class="tab-holder">
+        <div id="tab-one">
+            Tab 1 content
+        </div>
+        <div id="tab-two">
+            Tab 2 Content
+        </div>
+        <div id="tab-three">
+            Tab 3 Content
+        </div>
+    </div>
+</div>
+```
+
+### Settings and Callbacks
+You can pass settings through to the plugin on setup or reinit. Currently supported options are:
+
+| Property				| Default		| Description
+| --------------------- | ------------- | -----------
+| links        			| .tab-link		| A selector which can be used to find the tab links, which when clicked will open the corresponding tab
+| content      			| .tab-content	| A selector which can be used to find the divs which hold the content of the tabs
+| activeclass  			| active		| The name of the class which will be added to the tab links which are active
+| disabledclass 		| disabled		| If a tab link has this class it will not be activated
+| data 					| related-tab	| The name of the html data attribute against a tab link which holds the id of the related content div
+| default 				| -				| Id of the tab content div to be shown be default e.g. #tab-one (the default tab can be set other ways)
+| beforeShowCallback 	| -				| The callback function to be fired before the tab is shown
+| afterShowCallback 	| -				| The callback function to be fired after the tab has shown
+
+The following code snippet shows how you would call the plugin with custom options
+
+```js
+$('.addTabs').addTabs({
+    'default' : '#tab-two',
+    beforeShowCallback : function($link, $content){
+        alert('The tab is about to show');
+    },
+});
+```
+
+### Trigger Events
+You can also alter add tabs by firing events. Currently supported events are:
+
+| Property				| Default		| Description
+| --------------------- | ------------- | -----------
+| addTab-show-id       	| -				| The id of the tab content you wish to show e.g. #tab-two
+| addTab-show-index     | -				| The index of the tab content that you wish to show, this is a 0 based index so passing 1 would open tab two
+| addTab-next       	| -				| Go to the next tab
+| addTab-prev       	| -				| Go to the previous tab
+
+The following code snippet shows how you would trigger an event:
+
+```js
+$('.addTabs').trigger('addTab-show-id', "#tab-two");
+```
+
+### Subscribe to Events
+You can listen out for a tab change using the following snippet:
+
+```js
+$('.addTabs').on("addTab-show", function(event, link, content){
+    console.log(link);
+    console.log(content);
+});
+```
+
+* link: a jQuery object representing the active link
+* content: a jQuery object representing currently visible tab content
+
+### Re-init addTabs
+If you want to change the plugins options once it has been run use the following function
+
+```js
+$('.addTabs').data('addTabs').reInit({
+	'activeClass' : 'test'
+});
+```
+
+** Note: ** Only options you want to change need to be passed.
+
+### Destroy addTabs
+If you need to destroy the plugin, stop it listening for clicks and events and stop it triggering events and callback (may be useful for a responsive site) the call the destroy function as shown below
+
+```js
+$('.addTabs').data('addTabs').destroy();
+```
---
+++ src/jquery.addTabs.js
@@ -0,0 +1,264 @@
+(function($) {
+    $.addTabs = function(elm, options) {
+        var at = this;
+        var $elm = $(elm);
+
+        at.settings = {};
+        at.activeTab;
+        at.$links;
+        at.$content;
+        at.initRun = false;
+        at.currentIndex;
+        at.$elm = $elm;
+        
+        /**
+         * the "constructor" method that gets called when the object is created
+         */
+        at.init = function() {
+            at.settings = $.extend({}, $.fn.addTabs.options, options, $elm.data());
+            at.run();
+        };
+
+        /**
+         * Run the plugin
+         */
+        at.run = function() {
+            if(!at.initRun){
+                at.$links = $elm.find(at.settings.links);
+                at.$content = $elm.find(at.settings.content);
+                at.reset();
+                // run
+                if(at.$links.length && at.$content.length){
+                    at.showInit();
+                    at.addClick();
+                    at.addEvents();
+                    at.initRun = true;
+                }
+            }
+        }
+
+        /**
+         * add the click function to the links
+         */
+        at.addClick = function() {
+            at.$links.bind("click.at", function(e) {
+                e.preventDefault();
+                var $link = $(this);
+                at.showTab($link);
+            });
+        };
+
+        /**
+         * add public events
+         */
+        at.addEvents = function() {
+            $elm.on("addTab-show-id", function(event, id) {
+                at.showById(id);
+            });
+            $elm.on("addTab-show-index", function(event, index) {
+                at.showByIndex(index);
+            });
+            $elm.on("addTab-next", function(event) {
+                at.next();
+            });
+            $elm.on("addTab-prev", function(event) {
+                at.prev();
+            });
+        }
+
+        /**
+         * Show the initial tab
+         * This could be defined in the setting, by a default data attribute or by the default index (0)
+         */
+        at.showInit = function() {
+            var $link,
+                $defaultLink = at.findLinkByDefault();
+            if($defaultLink.length){
+                $link = $defaultLink;
+            }
+            else if(at.settings.default){
+                $link = at.findLinkByData(at.settings.default);
+            }
+            else{
+                $link = at.findLinkByIndex(0);
+            }
+            at.showTab($link);
+        };
+
+        /**
+         * Shows a tab
+         * @param link - jquery link element
+         */
+        at.showTab = function($link) {
+            var id = $link.data(at.settings.data),
+                index = at.$links.index($link),
+                $content = at.$content.filter(id);
+                
+            if(index != at.currentIndex && $content.length && !$link.hasClass(at.settings.disabledclass)){
+                // Before show callback fire
+                at.settings.beforeShowCallback.call(at, $link, $content);
+
+                at.reset();
+                $link.addClass(at.settings.activeclass);
+                $content.show();
+                at.currentIndex = index;
+                
+                // After show callback fire
+                at.settings.afterShowCallback.call(at, $link, $content);
+                $elm.trigger("addTab-show", [$link, $content])
+            }
+        };
+
+        /**
+         * Show a tab by id
+         * @param id - element id
+         */
+        at.showById = function(id){
+            $link = at.findLinkByData(id);
+            if($link.length){
+                at.showTab($link);
+            }
+        }
+
+        /**
+         * Show a tab by index
+         * @param index - zero based index of the tab
+         */
+        at.showByIndex = function(index){
+            $link = at.findLinkByIndex(index);
+            if($link.length){
+                at.showTab($link);
+            }
+        }
+
+        /**
+         * finds the link based on the related-data attribute
+         * @param data - data value
+         */
+        at.findLinkByData = function(data){
+            return at.$links.filter('[data-' + at.settings.data + '="' + data + '"]');
+        };
+
+        /**
+         * finds the link based on the default data attribute
+         */
+        at.findLinkByDefault = function(){
+            return at.$links.filter('[data-default-tab]').first();
+        };
+
+        /**
+         * finds the link based on an index
+         * @param index - index value of the link (0 based)
+         */
+        at.findLinkByIndex = function(index){
+            return at.$links.eq(index);
+        };
+
+        /**
+         * move to the next tab
+         */
+        at.next = function(){
+            var newIdx = at.currentIndex + 1;
+            at.manageChange(newIdx);
+        }
+
+        /**
+         * move to the prev tab
+         */
+        at.prev = function(){
+            var newIdx = at.currentIndex - 1;
+            at.manageChange(newIdx);
+        }
+
+        /**
+         * manage next, prev event
+         * @param newIdx new index
+         */
+        at.manageChange = function(newIdx) {
+            var total = at.getTotal();
+            if(newIdx > total){
+                newIdx = 0;
+            }
+            if(newIdx < 0){
+                newIdx = total;
+            }
+            var $link = at.findLinkByIndex(newIdx);
+            at.showTab($link);
+        }
+
+        /**
+         * helper function to get total slides
+         */
+        at.getTotal = function (){
+            return at.$links.length-1;
+        };
+
+        /**
+         * reset to the initial state
+         */
+        at.reset = function() {
+            at.$content.hide();
+            at.$links.removeClass(at.settings.activeclass);
+        };
+
+        /**
+         * Destroy the plugin
+         */
+        at.destroy = function() {
+            at.$content.show();
+            at.$links.removeClass(at.settings.activeclass);
+            at.$links.unbind("click.at");
+            at.removeEvents();
+            at.currentIndex = undefined;
+            at.initRun = false;
+        };
+
+        /**
+         * remove public events
+         */
+        at.removeEvents = function() {
+            $elm.unbind("addTab-show-id");
+            $elm.unbind("addTab-show-index");
+            $elm.unbind("addTab-next");
+            $elm.unbind("addTab-prev");
+        }
+
+        /**
+         * Reinitialise the plugin with new options 
+         * @param newOptions object
+         */
+        at.reInit = function(newOptions) {
+            if(at.initRun){
+                at.destroy();
+            }
+            at.settings = $.extend(at.settings, newOptions);
+            at.run();
+        }
+
+        // call the "constructor" method
+        at.init();
+    };
+
+    // add the plugin to the jQuery.fn object
+    $.fn.addTabs = function(options) {
+        // iterate through the DOM elements we are attaching the plugin to
+        return this.each(function() {
+            // if plugin has not already been attached to the element
+            if (undefined === $(this).data('addTabs')) {
+                var plugin = new $.addTabs(this, options);
+                $(this).data('addTabs', plugin);
+            }
+        });
+    };
+
+    $.fn.addTabs.options = {
+        'links'             : '.tab-link',
+        'content'           : '.tab-content',
+        'activeclass'       : 'active',
+        'disabledclass'     : 'disabled',
+        'data'              : 'related-tab',
+        'default'           : '',
+        beforeShowCallback  : function() {}, // function($link, $content) { }
+        afterShowCallback   : function() {}, // function($link, $content) { }
+    };
+})(window.jQuery || window.Zepto);
